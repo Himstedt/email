c4b1473eeaaeeed411ef3fb020bcf9dad267b516
Himstedt <106549888+Himstedt@users.noreply.github.com>
2022-06-05 14:13:49 +0200

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,34 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+subject = input("Title: ")
+body = input("Your text: ")
+sender_email = input("your email adress: ")
+receiver_email = input("target email adress: ")
+password = input("Enter a password: ")
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.set_content(body)
+
+html = f"""
+<html>
+    <body>
+        <h1>{subject}</h1>
+        <p>{body}</p>
+    </body>
+</html>
+"""
+
+message.add_alternative(htnl, subtype="html")
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+
+print("Success")
